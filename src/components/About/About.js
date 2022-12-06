fa913256db4ad87a7ccb08765888f13100cf6e31
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-12-06 14:24:54 -0800

    dark mode fix, resume update, overall wording fixes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,27 +5,27 @@ import { about } from '../../portfolio'
 import './About.css'
 
 const About = () => {
-  const {photo, name, role, description, resume, social } = about
+  const { photo, name, role, description, resume, social } = about
 
   return (
     <div className='about center'>
       {photo && (
-       <img height="300px" alt="Avatar placeholder" src={photo} />
+        <img height="300px" alt="Avatar placeholder" src={photo} />
       )}
       {name && (
         <h1>
-          Hi, I am <span className='about__name'>{name}.</span>
+           <span className='about__name'>{name}</span>
         </h1>
       )}
 
-      {role && <h2 className='about__role'>A {role}.</h2>}
+      {role && <h2 className='about__role'>A {role}</h2>}
       <p className='about__desc'>{description && description}</p>
 
       <div className='about__contact center'>
         {resume && (
           <a href={resume}
-          aria-label='resume'
-          className='link link--icon'
+            aria-label='resume'
+            className='link link--icon'
           >
             {/* <span type='button' className='btn btn--outline'>
               Resume
